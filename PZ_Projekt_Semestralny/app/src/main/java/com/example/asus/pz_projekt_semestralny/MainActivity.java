9eb99de9497ed787ed4c1cacbbffa3fbfc229a38
Hippo14 <hippo14@gmail.com>
2015-05-08 12:31:27 +0200

    v 0.01
    Dodawanie dynamicznej tablicy przycisków

--- PZ_Projekt_Semestralny/app/src/main/java/com/example/asus/pz_projekt_semestralny/MainActivity.java
+++ PZ_Projekt_Semestralny/app/src/main/java/com/example/asus/pz_projekt_semestralny/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.asus.pz_projekt_semestralny;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
+    TableLayout tbButtons;
+    String[] mTextofButton = { "Dipak", "E", "I", "J", "L",
+            "M", "G", "R", "N", "T", "H", "P",
+            "K", "Y", "V" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        tbButtons = (TableLayout) findViewById(R.id.tbButtons);
+
+        TableRow tr = new TableRow(this);
+        for (int i = 0; i < mTextofButton.length; i++) {
+            Button btn = new Button(this);
+            btn.setText(mTextofButton[i]);
+            tr.addView(btn);
+        }
+
+        tbButtons.addView(tr);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
